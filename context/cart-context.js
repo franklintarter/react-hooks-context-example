0e9cd4dc8dc5a8e6598f9369d5231f2944089249
Franklin Tarter <franklintarter@gmail.com>
2020-03-08 15:08:03 -0700

    init context

---
+++ context/cart-context.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const CartContext = React.createContext()
+
+const CartProvider = ({ children }) => {
+
+  const [fruit, setFruit] = useState('Apple')
+
+  const value = { fruit }
+
+  return (
+    <CartContext.Provider value={value}>
+      { children }
+    </CartContext.Provider>
+  )
+}
+
+export { CartContext, CartProvider }
